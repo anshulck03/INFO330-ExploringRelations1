d41bd6ca8d0df4d049d4558b8bec38be25348f78
Anshul Karanam <125226129+anshulck03@users.noreply.github.com>
2023-04-06 20:55:54 -0700

    Add files via upload

---
+++ extra-2.sql
@@ -0,0 +1,3 @@
+select age from persons where gender = 'Female' and title = 'Dr' and city = 'West Palm Beach' and state = 'Florida';
+
+select age from persons where gender = 'Male' and title = 'Reverend' and city = 'Phoenix' and state = 'Arizona';
---
+++ extra-3.sql
@@ -0,0 +1,5 @@
+select avg(age) from persons;
+
+select min(age) from persons;
+
+select max(age) from persons;
